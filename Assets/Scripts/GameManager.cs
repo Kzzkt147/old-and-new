719059f938580ffe358d40284ae56796738f5cde
Kzzkt147 <79820324+Kzzkt147@users.noreply.github.com>
2023-05-13 22:43:24 +1200

    Add quit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set;}
-    
+
+    private PlayerInputActions _playerInputActions;
     
     public void ResumeGame() => GameEvents.ResumeGame();
     public void PauseGame() => GameEvents.PauseGame();
@@ -12,9 +15,16 @@ public class GameManager : MonoBehaviour
     public void ChangeToFuture() => TimeSwitchController.ChangeTimePeriodToFuture();
     public void ChangeToPast() => TimeSwitchController.ChangeTimePeriodToPast();
 
-
+    private void Quit(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
     private void Awake()
     {
+        _playerInputActions = new PlayerInputActions();
+        
         if (Instance != null && Instance != this)
         {
             Debug.Log("Found a second instance of this singleton, destroying copycat...", gameObject);
@@ -24,5 +34,19 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+    }
+
+    private void OnEnable()
+    {
+        _playerInputActions.Utility.Enable();
+
+        _playerInputActions.Utility.Quit.performed += Quit;
+    }
+
+    private void OnDisable()
+    {
+        _playerInputActions.Utility.Disable();
+
+        _playerInputActions.Utility.Quit.performed -= Quit;
     }
 }
